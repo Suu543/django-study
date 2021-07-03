873508db8ab41e79ed5c051548755ad209429fce
jos50275266 <jos50275266@gmail.com>
2021-07-03 18:05:47 +0900

    Course Project - Building a Blog: Data & models 1. Planning the Data models 2. Adding a Post model 3. Author Model & One-to-many 4. Tag Model many-to-many 5. Registering Models for admin 6. Migrations & Admin Login 7. Adding Data via Admin 8. Configuring the Admin Panel 9. Fetching Posts for Starting Page 10. All Posts & Single Post Pages 11. Using Author & Tags Data

---
+++ my_site/Plan.md
@@ -0,0 +1,23 @@
+# Planning the Data Models
+
+Author
+- First Name
+- Last Name
+- Email Address
+
+One-to-Many
+
+Post
+- Title
+- Excerpt
+- Image Name
+- Date
+- Slug
+- Content
+
+Many-to-Many
+
+Tag
+- Caption
+
+python manage.py createsuperuser
--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,75 +1,106 @@
 from datetime import date
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect, Http404
 from django.template.loader import render_to_string
+from .models import Post
 
-all_posts = [
-    {
-        "slug": "hike-in-the-mountain",
-        "image": "mountains.jpg",
-        "author": "Yongsu",
-        "date": date(2021, 7, 21),
-        "title": "Mountain Hiking",
-        "excerpt": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima exercitationem ut id voluptatem soluta cum cumque voluptatibus",
-        "content": """
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-        """
-    },
-    {
-        "slug": "coding-in-the-mountain",
-        "image": "coding.jpg",
-        "author": "Yongsu",
-        "date": date(2020, 7, 21),
-        "title": "Mountain Hiking",
-        "excerpt": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima exercitationem ut id voluptatem soluta cum cumque voluptatibus",
-        "content": """
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-        """
-    },
-    {
-        "slug": "woods-in-the-mountain",
-        "image": "woods.jpg",
-        "author": "Yongsu",
-        "date": date(2019, 7, 21),
-        "title": "Mountain Hiking",
-        "excerpt": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima exercitationem ut id voluptatem soluta cum cumque voluptatibus",
-        "content": """
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-        """
-    }
-]
 
 def get_date(post):
     return post['date']
 
-# Create your views here.
-def starting_page(request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]
-    return render(request, "blog/index.html", {
-        "posts": latest_posts
-    })        
 
 def posts(request):
+    all_posts = Post.objects.all().order_by("-date")
     return render(request, "blog/all-posts.html", {
         "all_posts": all_posts
     })
 
+
+def starting_page(request):
+    latest_posts = Post.objects.all().order_by("-date")[:3]
+    return render(request, "blog/index.html", {
+        "posts": latest_posts
+    })
+
+
 def post_detail(request, slug):
     try:
-        identified_post = next(post for post in all_posts if post['slug'] == slug)
+        identified_post = get_object_or_404(Post, slug=slug)
         return render(request, "blog/post-detail.html", {
-            "post": identified_post
+            "post": identified_post,
+            "post_tags": identified_post.tags.all()
         })
+
     except:
         response_data = render_to_string("404.html")
         return HttpResponseNotFound(response_data)
+
+# Create your views here.
+# def get_date(post):
+#     return post['date']
+
+
+# def starting_page(request):
+#     sorted_posts = sorted(all_posts, key=get_date)
+#     latest_posts = sorted_posts[-3:]
+#     return render(request, "blog/index.html", {
+#         "posts": latest_posts
+#     })
+
+
+# def post_detail(request, slug):
+#     try:
+#         identified_post = next(
+#             post for post in all_posts if post['slug'] == slug)
+#         return render(request, "blog/post-detail.html", {
+#             "post": identified_post
+#         })
+#     except:
+#         response_data = render_to_string("404.html")
+#         return HttpResponseNotFound(response_data)
+
+
+# all_posts = [
+    #     {
+    #         "slug": "hike-in-the-mountain",
+    #         "image": "mountains.jpg",
+    #         "author": "Yongsu",
+    #         "date": date(2021, 7, 21),
+    #         "title": "Mountain Hiking",
+    #         "excerpt": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima exercitationem ut id voluptatem soluta cum cumque voluptatibus",
+    #         "content": """
+    #             Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+    #             Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+    #             Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+    #             Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+    #         """
+    #     },
+    #     {
+    #         "slug": "coding-in-the-mountain",
+    #         "image": "coding.jpg",
+    #         "author": "Yongsu",
+    #         "date": date(2020, 7, 21),
+    #         "title": "Mountain Hiking",
+    #         "excerpt": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima exercitationem ut id voluptatem soluta cum cumque voluptatibus",
+    #         "content": """
+    #             Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+    #             Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+    #             Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+    #             Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+    #         """
+    #     },
+    #     {
+    #         "slug": "woods-in-the-mountain",
+    #         "image": "woods.jpg",
+    #         "author": "Yongsu",
+    #         "date": date(2019, 7, 21),
+    #         "title": "Mountain Hiking",
+    #         "excerpt": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima exercitationem ut id voluptatem soluta cum cumque voluptatibus",
+    #         "content": """
+    #             Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+    #             Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+    #             Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+    #             Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+    #         """
+    #     }
+    # ]
